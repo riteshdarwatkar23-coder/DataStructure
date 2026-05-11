3304837df248e151c184bf23f6cc09e10bc3c64e
Ritesh <riteshdarwatkar23@gmail.com>
2026-05-11 17:05:14 +0530

    This my first BubbleSort python program

---
+++ Python/BubbleSort.py
@@ -0,0 +1,21 @@
+def main():
+    n = int(input("Enter number of elements: "))
+    arr = []
+
+    for i in range (n):
+        value = int(input("Enter values: "))
+        arr.append(value)
+
+    print("Array before sorting: ", arr)
+    bubble_sort(arr)
+    print("Array after sorting: ", arr)
+
+
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n-1):
+        for j in range(n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j],arr[j+1] = arr[j+1],arr[j]
+
+main()
