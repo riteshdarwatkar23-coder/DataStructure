775d44ef34ab1509509147d74e021d6be65d7cb1
Ritesh <riteshdarwatkar23@gmail.com>
2026-05-11 16:20:54 +0530

    This Is my first C Program

---
+++ C Programs/HelloWorld.c
@@ -0,0 +1,14 @@
+
+/**
+    Author : Rithesh 
+    Description : HelloWorld Program
+    Date : 11 May 2026
+
+*/
+#include<stdio.h>
+
+int main(void) {
+
+    printf("Hello World");
+    retrun 0;
+}
