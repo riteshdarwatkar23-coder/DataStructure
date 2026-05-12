d1adacbf17bcc59455b29c0d7e9fe4e684f1c68f
Ritesh <riteshdarwatkar23@gmail.com>
2026-05-12 15:49:57 +0530

    this my first program of QuickSort in python

---
+++ Python/QuickSort.py
@@ -0,0 +1,30 @@
+def partition(arr,low,high):
+    pivot = arr[high]
+    i = low - 1
+    for j in range(low,high):
+        if arr[j] <= pivot:
+            i+=1
+            arr[i],arr[j] = arr[j],arr[i]
+    arr[i+1],arr[high] = arr[high],arr[i+1]
+    return i+1
+
+def quick_sort(arr,low,high):
+    if low < high:
+        p = partition(arr,low,high)
+        quick_sort(arr,low,p-1)
+        quick_sort(arr,p+1,high)
+
+def main():
+    n = int(input("Enter number of elements: "))
+    arr = []
+    for i in range(n):
+    
+        values = int(input("Enter values: "))
+        arr.append(values)
+
+    print("Array before sorting: ",arr)
+    quick_sort(arr,0,len(arr)-1)
+    print("Array after sorting: ",arr)
+
+
+main()
