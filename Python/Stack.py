efce68367cbff2fef0dcb4612a21b00681f12ad4
Ritesh <riteshdarwatkar23@gmail.com>
2026-05-16 14:14:29 +0530

    This is my first stack code in python

---
+++ Python/Stack.py
@@ -0,0 +1,17 @@
+stack = []
+stack.append('a')
+stack.append('b')
+stack.append('c')
+
+print('Initial stack')
+print(stack)
+
+print("\n Elements popped from stack: ")
+print(stack.pop())
+print(stack.pop())
+print(stack.pop())
+
+
+print('\n stack after elements are popped: ')
+print(stack)
+
